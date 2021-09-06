31926d3c1b11dc16c0933ad7f0560899337d2764
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-09-07 02:00:27 +0530

    Home, About, Help, Weather and 404 routes added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('', (req, res) => {
-  res.send('Hello There!');
+  res.send('Home Page')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/help', (req, res) => {
+  res.send('Help Page')
+})
+
+app.get('/weather', (req, res) => {
+  res.send('Weather Page')
+});
+
+app.get('/help/*', (req, res) => {
+  res.send('Help Resource not Found')
+})
+
+app.get('*', (req, res) => {
+  res.send('404 - Page not Found')
 })
 
 app.listen(PORT, () => {
