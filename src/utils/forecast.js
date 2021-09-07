de39a47b695469d99913e3a6e4078e66f46513bd
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-09-07 20:41:41 +0530

    Humidity feature added.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude,callback) => {
-  const forecastUrl = `http://api.weatherstack.com/current?access_key=ab3746950c0a4321a62df&query=${latitude},${longitude}&units=m`
+  const forecastUrl = `http://api.weatherstack.com/current?access_key=ab37469500e26e9b4070c0a4321a62df&query=${latitude},${longitude}&units=m`
 
   request({ url: forecastUrl, json: true },(error,{ body } = {}) => {
     //This will run if there is an OS level error i.e. No internet
@@ -15,7 +15,7 @@ const forecast = (latitude,longitude,callback) => {
       return
     }
     //This will run if there is no error
-    callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out but feels like ${body.current.feelslike} degrees`)
+    callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out but feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%`)
   })
 }
 
